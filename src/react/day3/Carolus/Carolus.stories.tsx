aa37275f99e6517d32ee0ed38ffde296825b620d
David <david@krawaller.se>
2020-09-11 07:12:41 +0200

    move carolus example

--- src/react/day3/Carolus/Carolus.stories.tsx
+++ src/react/day3/Carolus/Carolus.stories.tsx
@@ -5,7 +5,7 @@ import { Carolus as CarolusModule } from './Carolus.Module'
 import { Carolus as CarolusInline } from './Carolus.Inline'
 import { Carolus as CarolusGlitz } from './Carolus.Glitz'
 
-storiesOf('Day 1/Styling/Carolus', module)
+storiesOf('Day 3/Styling/Carolus', module)
   .add('global CSS', () => <CarolusGlobal />)
   .add('module', () => <CarolusModule />)
   .add('inline', () => <CarolusInline />)
